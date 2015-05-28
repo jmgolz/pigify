c424884c0cf836d454fd72d53bbe2e62ac64ab19
= <=>
2015-05-28 00:39:45 -0400

    update tests to match return expected conds

--- test_pigify.rb
+++ test_pigify.rb
@@ -31,9 +31,16 @@ class TestPiglatinTranslate < Minitest::Test
 	end
 
 	def test_sentence_with_numbers
-		#what we should get "15 Angerousday Actsfay Electriciansyay Eepkay Otay Emselvesthay"
+		#what we should get "15 angerousDay actsFay Electriciansay eepKay oTay hemselvesTay"
 		input = "15 Dangerous Facts Electricians Keep To Themselves"
 
 		assert_equal("15 angerousDay actsFay Electriciansay eepKay oTay hemselvesTay", @translate.human_readable_string_to_piglatin(input))
+	end
+
+	def test_sentence_with_punctuation
+		#what we should get "15 angerousDay actsFay Electriciansay eepKay oTay hemselvesTay"
+		input = "The unpopular glory renames an ice!"
+
+		assert_equal("Ethay unpopularyay oryglay enamesray anyay iceyay!", @translate.human_readable_string_to_piglatin(input))
 	end
 end
